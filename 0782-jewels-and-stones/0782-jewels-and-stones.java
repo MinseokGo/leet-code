aa1d938a655587183fa594e0e068cb0a767c911a
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2025-04-22 18:46:54 +0900

    Time: 0 ms (100%), Space: 41.6 MB (88.93%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -2,14 +2,9 @@ class Solution {
 
     public int numJewelsInStones(String jewels, String stones) {
         int answer = 0;
-        Set<Character> set = new HashSet<>();
-
-        for (char jewel : jewels.toCharArray()) {
-            set.add(jewel);
-        }
 
         for (char stone : stones.toCharArray()) {
-            if (set.contains(stone)) {
+            if (jewels.indexOf(stone) != -1) {
                 answer += 1;
             }
         }
