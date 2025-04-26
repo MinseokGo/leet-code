64c6eb19aa1207abd731bacf095cab182ecb19af
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2025-04-26 14:48:11 +0900

    Time: 3 ms (84.63%), Space: 49.7 MB (52.75%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,16 +1,21 @@
 class Solution {
 
     private static final int[] dx = {-1, 1, 0, 0};
-    private static final int[] dy = {0, 0, -1, 1}; 
+    private static final int[] dy = {0, 0, -1, 1};
+
+    private static int n;
+    private static int m;
 
     public int numIslands(char[][] grid) {
         int island = 0;
+        n = grid.length;
+        m = grid[0].length;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1') {
                     island += 1;
-                    bfs(grid, i, j);
+                    dfs(grid, i, j);
                 }
             }
         }
@@ -18,36 +23,20 @@ class Solution {
         return island;
     }
 
-    private void bfs(char[][] grid, int x, int y) {
-        Queue<Position> queue = new LinkedList<>();
-        queue.add(new Position(x, y));
+    private void dfs(char[][] grid, int x, int y) {
+        grid[x][y] = '0';
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-        while (!queue.isEmpty()) {
-            Position position = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = position.x + dx[i];
-                int ny = position.y + dy[i];
-
-                if (isValidPosition(nx, ny, grid.length, grid[0].length) && grid[nx][ny] == '1') {
-                    queue.add(new Position(nx, ny));
-                    grid[nx][ny] = 1;
-                }
+            if (isValidPosition(nx, ny) && grid[nx][ny] == '1') {
+                dfs(grid, nx, ny);
             }
         }
     }
 
-    private boolean isValidPosition(int x, int y, int mapX, int mapY) {
-        return x >= 0 && y >= 0 && x < mapX && y < mapY;
-    }
-
-    private static class Position {
-        int x;
-        int y;
-
-        Position(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+    private boolean isValidPosition(int x, int y) {
+        return x >= 0 && y >= 0 && x < n && y < m;
     }
 }
