de34f46c657b92cdebddf0d986c086a797d351d1
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2025-04-26 14:38:13 +0900

    Time: 6 ms (24.56%), Space: 52.2 MB (21.58%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,58 @@
+class Solution {
+
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1}; 
+
+    public int numIslands(char[][] grid) {
+        int island = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    island += 1;
+                    bfs(grid, i, j, visited);
+                }
+            }
+        }
+
+        return island;
+    }
+
+    private void bfs(char[][] grid, int x, int y, boolean[][] visited) {
+        Queue<Position> queue = new LinkedList<>();
+        queue.add(new Position(x, y));
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            Position position = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = position.x + dx[i];
+                int ny = position.y + dy[i];
+
+                if (isValidPosition(nx, ny, grid.length, grid[0].length) 
+                    && !visited[nx][ny] 
+                    && grid[nx][ny] == '1'
+                ) {
+                    queue.add(new Position(nx, ny));
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
+
+    private boolean isValidPosition(int x, int y, int mapX, int mapY) {
+        return x >= 0 && y >= 0 && x < mapX && y < mapY;
+    }
+
+    private static class Position {
+        int x;
+        int y;
+
+        Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
