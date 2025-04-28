0141b04a8d8f0e25ddf3f1b0a2973daeccc02717
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2025-04-28 15:26:16 +0900

    Time: 1 ms (93.34%), Space: 44.6 MB (46.29%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,31 +1,35 @@
 class Solution {
 
     private int length;
-    private boolean[] visited;
     private List<List<Integer>> combinations = new ArrayList<>();
 
     public List<List<Integer>> permute(int[] nums) {
         length = nums.length;
-        visited = new boolean[length];
 
-        dfs(nums, new ArrayList<>());
+        dfs(nums, 0);
         return combinations;
     }
 
-    private void dfs(int[] nums, List<Integer> combination) {
-        if (combination.size() == length) {
-            combinations.add(new ArrayList<>(combination));
+    private void dfs(int[] nums, int start) {
+        if (start == length) {
+            List<Integer> combination = new ArrayList<>();
+            for (int num : nums) {
+                combination.add(num);
+            }
+            combinations.add(combination);
             return;
         }
 
-        for (int i = 0; i < length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                combination.add(nums[i]);
-                dfs(nums, combination);
-                visited[i] = false;
-                combination.remove(combination.size() - 1);
-            }
+        for (int i = start; i < length; i++) {
+            swap(nums, i, start);
+            dfs(nums, start + 1);
+            swap(nums, i, start);
         }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
